911166623db6f878d562aed79a39175a5b425e1b
Ahryxx <phvanthanh06@gmail.com>
2024-11-17 15:28:23 +0700

    17/11/24

---
+++ src/main/java/controller/admin/UserController.java
@@ -0,0 +1,135 @@
+package vn.hoidanit.laptopshop.controller.admin;
+
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UploadService;
+import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+    private final UploadService uploadService;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(
+            UploadService uploadService,
+            UserService userService,
+            PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.uploadService = uploadService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        List<User> arrUsers = this.userService.getAllUserByEmail("redcream2004@gmail.com");
+        System.out.println(arrUsers);
+        model.addAttribute("Ahryxx", "test");
+        model.addAttribute("Pvt", "From controller with model");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUser();
+        model.addAttribute("users1", users);
+        return "admin/user/show";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "admin/user/detail";
+    }
+
+    @GetMapping("/admin/user/create") // GET
+    public String getCreateUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @PostMapping(value = "/admin/user/create")
+    public String CreateUserPage(Model model,
+            @ModelAttribute("newUser") @Valid User ahryxx,
+            BindingResult newUserBindingResult,
+            @RequestParam("ahryxxFile") MultipartFile file) {
+
+        List<FieldError> errors = newUserBindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(error.getField() + " - " + error.getDefaultMessage());
+        }
+
+        // Validate
+        if (newUserBindingResult.hasErrors()) {
+            return "/admin/user/create";
+        }
+
+        String hashPassword = this.passwordEncoder.encode(ahryxx.getPassword());
+
+        // Image
+        String avatar = this.uploadService.handleSaveUpLoadFile(file, "avatar");
+        ahryxx.setAvatar(avatar);
+        ahryxx.setPassword(hashPassword);
+        ahryxx.setRole(this.userService.getRoleByName(ahryxx.getRole().getName()));
+        // save
+        this.userService.handleSaveUser(ahryxx);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping("/admin/user/update/{id}") // GET
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
+        User currentUser = this.userService.getUserById(id);
+        model.addAttribute("newUser", currentUser);
+        return "admin/user/update";
+    }
+
+    @PostMapping("/admin/user/update")
+    public String getUpdateUserPage(Model model, @ModelAttribute("newUser") User ahryxx,
+            @RequestParam("ahryxxFile") MultipartFile file) {
+        String avatar = this.uploadService.handleSaveUpLoadFile(file, "avatar");
+        User currentUser = this.userService.getUserById(ahryxx.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(ahryxx.getAddress());
+            currentUser.setFullName(ahryxx.getFullName());
+            currentUser.setPhone(ahryxx.getPhone());
+            currentUser.setRole(this.userService.getRoleByName(ahryxx.getRole().getName()));
+            currentUser.setAvatar(avatar);
+
+            this.userService.handleSaveUser(currentUser);
+        }
+        return "redirect:/admin/user";
+    }
+
+    @GetMapping("/admin/user/delete/{id}")
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        // User user = new User();
+        // user.setId(id);
+        model.addAttribute("newUser", new User());
+        return "admin/user/delete";
+    }
+
+    @PostMapping("/admin/user/delete")
+    public String postDeleteUser(Model model, @ModelAttribute("newUser") User ahryxx) {
+        this.userService.deleteAUser(ahryxx.getId());
+        return "redirect:/admin/user";
+    }
+}
---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,66 @@
+package vn.hoidanit.laptopshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.laptopshop.domain.Role;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
+import vn.hoidanit.laptopshop.repository.RoleRepository;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserService(UserRepository userRepository,
+            RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    public String handleHello() {
+        return "Hello from Service";
+    }
+
+    public List<User> getAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmail(String email) {
+        return this.userRepository.findOneByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        User ahryxx = this.userRepository.save(user);
+        System.out.println(ahryxx);
+        return ahryxx;
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void deleteAUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    public Role getRoleByName(String name) {
+        return this.roleRepository.findByName(name);
+    }
+
+    public User registerDTOtoUser(RegisterDTO registerDTO) {
+        User user = new User();
+
+        user.setFullName(registerDTO.getFirstName() + " " + registerDTO.getLastName());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(registerDTO.getPassword());
+        user.setAddress("");
+        user.setAvatar("");
+        user.setPhone("");
+        user.setRole(getRoleByName("USER"));
+        return user;
+    }
+}
